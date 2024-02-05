45b1017bd59cc2885d27471ab045d39ab7edd204
Isha Kohli <ishakohli252001@gmail.com>
2024-02-05 20:05:48 +0530

    Deploy

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCzOwnnMN17SF5UbFK9l6esG0hQmZZAozo",
+    authDomain: "keeper-app-d6780.firebaseapp.com",
+    projectId: "keeper-app-d6780",
+    storageBucket: "keeper-app-d6780.appspot.com",
+    messagingSenderId: "724833243417",
+    appId: "1:724833243417:web:a361cb021898a5a5d4f588",
+    measurementId: "G-ZBF7PSX4Y1"
+  };
